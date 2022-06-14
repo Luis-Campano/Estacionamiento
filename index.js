5d395f233f93d0dba953477267c49b116e9783ab
Juan Vazquez <vazquez.juan.1fm@gmail.com>
2022-06-14 18:43:19 -0500

    Instalación y configuración

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//Rama para el inicio del desarrollo.
+const express = require('express'); 
+
+//Importación de variables de entorno.
+require('dotenv').config();
+
+//creación del servidor.
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Puerto de escuchar.
+app.listen(process.env.APP_PORT, () => {
+    console.log("El servidor esta, activo.");
+})
