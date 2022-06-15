d32fcb88012a594611e2c6d6dbaae20e9d2af9a8
Luis-Campano <luis.campano.esp@gmail.com>
2022-06-14 19:16:21 -0500

    Configuración BD

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express'); 
+const cors = require ('cors');
+const db = require('./models')
 
 //Importación de variables de entorno.
 require('dotenv').config();
+
+// conectar la BD
+db.sequelize.authenticate()
+.then(() => {
+  console.log("BD Conectada");
+})
+.catch((error) => {
+  console.log(error);
+});
 
 //creación del servidor
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// cors
+/*app.use(
+    cors({
+        credentials: true,
+        origin: ['http://localhost:5000'],
+    })
+);
+app.use('/', routes());
+*/
 
 //Puerto de escuchar.
 app.listen(process.env.APP_PORT, () => {
