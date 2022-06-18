2ad63b7a858c3071dcf36fc34a6878050ac80b92
sandraSalas <sandra1908hernandez@gmail.com>
2022-06-18 14:33:07 -0500

    creacion de las rutas

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+//importación de controlladores
+const customerController = require('./controllers/customerController');
+
+module.exports = () => {
+
+    ////Estados
+    router.get('/customer', customerController.list);
+    router.get('/customer/show/:id', customerController.show);
+    router.post('/customer', customerController.add);
+    router.put('/customer/update/:id', customerController.update);
+    router.delete('/customer/delete/:id', customerController.delete);
+    router.get('/customer/search/:id', customerController.search);
+}
