8639516d17d80cbaa1ce6b8b81bb67c5abc468f2
Juan Vazquez <vazquez.juan.1fm@gmail.com>
2022-08-18 12:52:51 -0500

    Cambios en codigo, deploy

--- config/config.js
+++ config/config.js
@@ -14,19 +14,19 @@ const config = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT
-    },/*
+    },
     production: {
         username: process.env.DB_USER_NAME,
         password: process.env.DB_USER_PWD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT
-    }*/
+    }/*
     production: {
         use_env_variable: process.env.CLEARDB_DATABASE_URL,
         dialect: process.env.DB_DIALECT,
         logging: true,
-    },
+    },*/
 };
 
 module.exports = config;
